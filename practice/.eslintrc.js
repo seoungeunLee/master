6086e6f91953b17709d574dcd19d7515a15399af
유민 <wkzkfdl1@gmail.com>
2023-06-03 11:55:11 +0900

    prettier, elint install

--- practice/.eslintrc.js
+++ practice/.eslintrc.js
@@ -1,21 +1,19 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'standard-with-typescript'
-  ],
-  overrides: [
-  ],
+  extends: ['plugin:react/recommended', 'plugin:prettier/recommended'],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
     ecmaVersion: 'latest',
-    sourceType: 'module'
+    sourceType: 'module',
   },
-  plugins: [
-    'react'
-  ],
+  plugins: ['react', '@typescript-eslint'],
   rules: {
-  }
-}
+    'prettier/prettier': 'error',
+  },
+};
